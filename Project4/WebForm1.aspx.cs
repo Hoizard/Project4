4c62f954e5bd770027872c9e852e384fe516e7e3
Erick Cruz <31190144+Hoizard@users.noreply.github.com>
2018-10-11 20:17:59 -0500

    Added code but does not work due to connection of IOFIle

--- Project4/WebForm1.aspx.cs
+++ Project4/WebForm1.aspx.cs
@@ -9,9 +9,77 @@ namespace Project4
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (RadioButtonList1.SelectedValue.ToLower() == "Other".ToLower())
+            {
+                OtherYears.Enabled = true;
+            }
+            else
+            {
+                OtherYears.Enabled = false;
+                OtherYears.Text = string.Empty;
+            }
+        }
 
+        protected double ComputeMonthlyPayment(double principal, double years, double rate)
+        {
+            double monthly = 0;
+            double top = principal * rate / 1200.00;
+            double bottom = 1 - Math.Pow(1.0 + rate / 1200.0, -12.0 * years);
+            monthly = top / bottom;
+            return monthly;
+        }
+
+        protected void ComputeMortgage_Click(object sender, EventArgs e)
+        {
+            string inPrin = PrincipleAmount.Text;
+            string inYears = OtherYears.Text;
+            string inRate = DropDownList1.SelectedItem.Text;
+
+            double years = 0;
+
+            bool check = false;
+
+            if (double.TryParse(inPrin, out double principal) == false)
+            {
+                ResultPayment.Text = $"Please input a Principle Amount";
+                check = true;
+            }
+
+            if (RadioButtonList1.SelectedIndex == 2)
+            {
+                if (double.TryParse(inYears, out years) == false)
+                {
+                    ResultPayment.Text = $"Please select an Loan Duration";
+                    check = true;
+                }
+            }
+            if (RadioButtonList1.SelectedIndex == 0)
+            {
+                years = 15;
+            }
+            if (RadioButtonList1.SelectedIndex == 1)
+            {
+                years = 30;
+            }
+
+            if (double.TryParse(inRate, out double rate) == false)
+            {
+                ResultPayment.Text = $"Please select an Interest Rate";
+                check = true;
+            }
+
+            if (check == false)
+            {
+                double monthly = ComputeMonthlyPayment(principal, years, rate);
+                string formattedOutput = string.Format("The monthly payment is {0:C}", monthly);
+                ResultPayment.Text = formattedOutput;
+
+                //IIOHelper fileIOHelper = new DatabaseIOHelper();
+                FileIOHelper fileIOHelper = new FileIOHelper();
+                fileIOHelper.AddMortgages(formattedOutput);
+            }
         }
     }
 }
