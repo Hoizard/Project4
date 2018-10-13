a7fc06f2f48ea82e8e3996951d048074c3102138
Erick Cruz <31190144+Hoizard@users.noreply.github.com>
2018-10-12 19:59:13 -0500

    Got timer to work without Post Back everytime

--- Project4/Site1.Master.designer.cs
+++ Project4/Site1.Master.designer.cs
@@ -58,6 +58,15 @@ namespace Project4 {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
         
         /// <summary>
+        /// UpdatePanel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+        
+        /// <summary>
         /// Timer1 control.
         /// </summary>
         /// <remarks>
